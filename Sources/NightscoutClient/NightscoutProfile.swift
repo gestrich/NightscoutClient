68aa6b82c1af600b382bfb1b67005f3c446b3542
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2022-11-13 16:33:06 -0500

    Make loopSettings from profile optional

--- Sources/NightscoutClient/NightscoutProfile.swift
+++ Sources/NightscoutClient/NightscoutProfile.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct NightscoutProfile: Codable {
     public let startDate: String
-    public let loopSettings: NightscoutLoopSettings
+    public let loopSettings: NightscoutLoopSettings?
 }
 
 public struct NightscoutLoopSettings: Codable {
